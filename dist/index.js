87397cf700e6dd867ad2762e397d89e8356e2e68
Yaser Almasri <yaser@masys.co>
2019-05-02 21:41:11 -0500

    set current user

--- dist/index.js
+++ dist/index.js
@@ -92,6 +92,8 @@ var Auth = function () {
     value: function removeStorage() {
       var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
+      this.setCurrentUser();
+
       Vue.delete(this, 'email');
       Vue.delete(this, 'currentUser');
       Vue.delete(this, 'acls');
